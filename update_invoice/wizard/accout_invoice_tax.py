422bbbd24de6b9417c52791f6c4220372a8b1c09
Aktiv Software <odoo@aktivsoftware.com>
2020-02-11 17:42:25 +0530

    [ADD]: Add code to validate Invoices.

--- update_invoice/wizard/accout_invoice_tax.py
+++ update_invoice/wizard/accout_invoice_tax.py
@@ -17,8 +17,15 @@ class InvoiceList(models.Model):
                 else:
                     taxes = res.product_id.supplier_taxes_id.filtered(lambda r: r.company_id == res.company_id) or res.account_id.tax_ids or res.invoice_id.company_id.account_purchase_tax_id
                 if res.invoice_id.fiscal_position_id:
-                  pass
+                    pass
                 res.invoice_line_tax_ids = res.invoice_id.fiscal_position_id.map_tax(taxes, res.product_id, res.invoice_id.partner_id)
             invoice._onchange_invoice_line_ids()
             invoice._compute_amount()
             invoice._onchange_partner_id()
+
+    def validate_invoice(self):
+        """Remove prduct name in description sale."""
+        context = dict(self._context or {})
+        invoices = self.env['account.invoice'].browse(context['active_ids'])
+        for invoice in invoices:
+            invoice.action_invoice_open()
