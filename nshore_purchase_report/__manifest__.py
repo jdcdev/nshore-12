3a64536b16e97c747a32c4a0a5b62cef68ea25a7
Aktiv software <odoo@aktivsoftware.com>
2020-12-21 14:50:38 +0530

    [ADD]: Added purchase order reports modifications module.

---
+++ nshore_purchase_report/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Nshore Purchase Report',
+    'version': '12.0.1.0.0',
+    'category': 'Partner',
+    'license': 'AGPL-3',
+    'author': 'JDC system',
+    'summary': 'Nshore Purchase Report',
+    'depends': [
+        'nshore_customization'
+    ],
+    'data': [
+        'report/purchase_order_template.xml',
+        'report/purchase_quotation_template.xml',
+        'report/picking_operations_template.xml',
+        'report/delivery_slip_template.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
