d69e4aabf925dbc703f46cddb86f2bc9135b9150
Aktiv Software <odoo@aktivsoftware.com>
2021-02-01 10:57:12 +0530

    [ADD]: Added salesperson access rights of readonly in sales order and invoice.

--- nshore_customization/models/invoice.py
+++ nshore_customization/models/invoice.py
@@ -79,6 +79,7 @@ class AccountInvoice(models.Model):
     pricelist_id = fields.Many2one(
         'product.pricelist', string='Pricelist',
         readonly=True, states={'draft': [('readonly', False)]})
+    user_id = fields.Many2one('res.users', readonly=False)
 
     @api.multi
     def action_invoice_open(self):
