ebddcc80ca79792d0d7c4ba062c1df8ed59633e0
Aktiv Software <odoo@aktivsoftware.com>
2021-02-02 12:50:40 +0530

    [ADD]: Added comments line.

--- nshore_customization/models/stock_inventory_line.py
+++ nshore_customization/models/stock_inventory_line.py
@@ -7,4 +7,5 @@ class InventoryLine(models.Model):
     _inherit = "stock.inventory.line"
     _order = 'name, product_id'
 
+    # Added products name related field to order lines by product name and id
     name = fields.Char("Name", related="product_id.name")
