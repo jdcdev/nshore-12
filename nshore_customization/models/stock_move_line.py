05a500c0ef6cfaa4a234e3b4f212b51b838df094
Aktiv Software <odoo@aktivsoftware.com>
2021-02-03 17:39:53 +0530

    [UPDATE]: Update order sequence for products, template and category by alphabetical order.

--- nshore_customization/models/stock_move_line.py
+++ nshore_customization/models/stock_move_line.py
@@ -5,5 +5,6 @@ from odoo import fields, models
 class StockQuant(models.Model):
     _inherit = 'stock.move.line'
     
+    # added field to get name of customer/vendor in move.
     partner_id = fields.Many2one(
         'res.partner', 'Customer/Vendor', related="picking_id.partner_id")
