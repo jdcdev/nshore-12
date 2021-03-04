f6d97cace6f287b112ff9e867ebf7c6162a80559
Aktiv Software <odoo@aktivsoftware.com>
2021-03-04 14:12:26 +0530

    [UPDATE]: Update Inventory Adjustment with modifictions in tree and form view.

--- nshore_customization/models/stock_inventory_line.py
+++ nshore_customization/models/stock_inventory_line.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
+from odoo import fields, models
+
+
+class StockInv(models.Model):
+    """Inherit Stock Inventory to add mail thread."""
+    _name = 'stock.inventory'
+    _inherit = ['stock.inventory', 'mail.thread', 'mail.activity.mixin']
 
 
 class InventoryLine(models.Model):
+    """Inherit Inventory Line module to add order."""
+
     _inherit = "stock.inventory.line"
     _order = 'name, product_id'
 
