93c90dcccc8c8f0272faa66696adc45a4b826f3b
Aktiv Software <odoo@aktivsoftware.com>
2021-03-15 18:15:36 +0530

    [ADD]: Added new module for tree view limit

---
+++ odoo_treeview_limit/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "Tree View Limit",
+
+    'summary': 'Expand the limitations of default tree view',
+    'description': """
+    This module helps users to see all the records in the list view.
+        """,
+
+    'author': 'JDC System',
+    'category': 'web',
+    'version': '12.0.1.0.0',
+    'license': "AGPL-3",
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base'
+    ],
+    # always loaded
+    'data': [
+        'views/res_company_view.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
