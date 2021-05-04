cbc6020b86ada2b21b157b39a33981db787a08fb
Aktiv Software <odoo@aktivsoftware.com>
2021-05-04 12:49:46 +0530

    [ADD]: Added import extend limit and freeze header for import preview screen.

--- nshore_customization/models/__init__.py
+++ nshore_customization/models/__init__.py
@@ -10,6 +10,7 @@ from . import stock_quant
 from . import pos_order
 from . import purchase_order
 from . import stock_move_line
+from . import base_import_count
 from . import account_aged_receivable
 from . import stock_inventory_line
 from . import product_pricelist
