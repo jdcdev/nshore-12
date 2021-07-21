81d1ba499d8faf3a8fa31787bd4e2da161fef02e
Odoo Aktiv <odoo@aktivsoftware.com>
2021-07-21 13:05:35 +0530

    [UPDATE] #13614 | sale_return | add sequence filed

---
+++ nshore_customization/models/portal_note.py
@@ -0,0 +1,8 @@
+# See LICENSE file for full copyright and licensing details.
+from odoo import api, fields, models
+
+
+class PortalWizard(models.Model):
+    """Class inherit to add some function."""
+
+    _inherit = 'portal.wizard'
