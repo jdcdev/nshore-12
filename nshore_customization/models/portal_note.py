7bcbe7c02aa7b4561d037f536d0377de151e211e
‘Odoo <odoo@aktivsoftware.com>
2021-07-22 16:17:17 +0530

    [ADD] #13668 | nshore_customization | add py file for portal and log note in contacts when grant portal access

--- nshore_customization/models/portal_note.py
+++ nshore_customization/models/portal_note.py
@@ -1,9 +1,20 @@
-# See LICENSE file for full copyright and licensing details.
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
-class PortalWizard(models.Model):
-    """Class inherit to add some function."""
+class PortalWizard(models.TransientModel):
+    """Class inherit to add log note for portal."""
 
     _inherit = 'portal.wizard'
 
+
+    @api.multi
+    def action_apply(self):
+        res = super(PortalWizard, self).action_apply()
+        names = self.user_ids.filtered(lambda x: x.in_portal).mapped('partner_id').mapped('display_name')
+        msg = """
+                %s Granted Poratl Access to:<br/>
+                <ul>%s</ul>""" % (self.env.user.name, ''.join(["<li>%s</li>" % x for x in names]))
+        active_ids = self._context.get('active_ids')
+        user = self.env['res.partner'].browse(active_ids)
+        user.message_post(body=msg)
+        return res 
